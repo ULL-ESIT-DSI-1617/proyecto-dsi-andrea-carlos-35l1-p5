2f6d1304f1d08a9c6586db93ba8783517dc40586
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-05-20 18:22:19 +0100

    Hecha base de datos con MongoDB

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt-nodejs');
 var session = require('express-session');
-var file = './user.json'
-var jsonfile = require('jsonfile');
-var users = require('./user.json');
 var comparar = require('@ull-andrea-carlos/comparar');
+var mongoose = require('mongoose');
+
 //permite coger parámetros de la url(query string)
 app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/usuarios', function(error){
+  if (error) {
+    throw error; 
+  } else {
+    console.log('Conectado a MongoDB');
+  }
+});
+
+// This is our mongoose model for todos
+var Schema = mongoose.Schema({
+    usuario: String,
+    contrasena: String
+});
+
+//Creamos el modelo a partir del squema
+var Usuarios = mongoose.model('Usuarios', Schema);
+
 
 app.use(session({
     secret: 'example',
@@ -17,17 +34,32 @@ app.use(session({
     saveUninitialized: true
 }));
 
-var registrar = function(user, pass){
-  users[user] = bcrypt.hashSync(pass);
-}
-
 //Comprueba si ya esta autorizado en esta sesion
 var auth = function(req, res, next) {
-  if (req.session && req.session.user in users)
-    return next();
-  else
-    return res.render('formulario/noautentificado');
-};
+
+  Usuarios.findOne({usuario: req.session.user}, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send("ERROR");
+    } else {
+      if (result != null) {
+
+        if (req.session) {
+          return next();
+        } else {
+          return res.sendStatus(401);
+        }
+
+      } else {
+        return res.sendStatus(401);
+      }
+
+    }
+
+  })
+
+}; 
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -38,25 +70,103 @@ app.get('/', function(req, res){
   res.render('formulario/noautentificado');
 });
 
-/*
-app.get('/', function(req, res){
-  res.render('noautentificado', { message: 'Para acceder al contenido es necesario autentificarse:' } );
-});
-*/
 //Muestra la vista con el formulario para log in
 app.get('/login', function(req, res){
   res.render('formulario/login');
 });
 
-//Genera el json con los usuarios especificados en la variable users
-
-jsonfile.writeFile(file, users, {spaces: 2}, (err)=>{
-	console.error(err);
-});
-
 app.get('/registrar', function(req, res){
   res.render('formulario/registrar')
 })
+
+var registrar = function (user, pass) {
+
+  console.log(user);
+  console.log(pass);
+
+  usuario1 = new Usuarios ({"usuario": user, "contrasena": bcrypt.hashSync(pass)}, function (err, result) {
+    if (err) return handleError(err);
+  })
+
+  usuario1.save (function (err) {
+    if (err) return handleError(err);
+  })
+
+};
+
+app.post('/registrar', function(req, res) {
+
+  if (!req.body.username || !req.body.password) { //campos imvalidos o nulos
+
+    console.log('registrar failed');
+    res.render('formulario/registrar');
+
+  } else {
+
+    Usuarios.findOne({usuario: req.body.username}, function (err, result) {
+      if (err) {
+        console.log(err);
+        res.send("ERROR");
+      } else {
+
+        if (result != null) {
+
+          console.log('Usuario ya registrado')
+          console.log(result.usuario)
+          res.render('formulario/registrar');
+
+        } else {
+
+          registrar(req.body.username, req.body.password);
+          res.render('formulario/noautentificado');
+
+        }
+
+      }
+
+    })
+  
+  }
+
+});
+
+app.post('/login', function(req, res) {
+
+  if (!req.body.username || !req.body.password) { //campos invalidos o nulos
+
+    console.log('Rellene los campos');
+    res.render('formulario/noautentificado');
+
+  } else {
+
+    Usuarios.findOne({usuario: req.body.username}, function (err, result) {
+      if (err) {
+        console.log(err);
+        res.send("ERROR");
+      } else {
+
+        if (result != null) {
+          
+          if (result.username = req.body.username && bcrypt.compareSync(req.body.password, result.contrasena)) {
+            req.session.user = req.body.username;
+            req.session.admin = true;
+            console.log("Usuario correcto");
+            console.log(result.usuario);
+            res.render('formulario/autentificado');
+          }
+
+        } else {
+          console.log('login failed');
+          res.render('formulario/noautentificado')
+        }
+
+      }
+
+    })
+
+  }
+
+});
 
 app.get('/comparar',auth, function(req, res){
   res.render('content/input', { result: ''})
@@ -68,39 +178,7 @@ app.post('/comparar', function(req, res){
   res.render('content/input', { result: resultado})
 })
 
-app.post('/registrar', function(req, res){
-  if (!req.body.username || !req.body.password) {
-    console.log('registrar failed');
-      res.render('formulario/registrar');
-  } else if(req.body.username in users) {
 
-    console.log('registrado fallido');
-    res.render('formulario/registrar');
-  } else {
-    registrar(req.body.username, req.body.password)
-    res.render('formulario/noautentificado');
-
-    jsonfile.writeFile(file, users, {spaces: 2}, (err)=>{
-      console.error(err);
-    });
-  }
-})
-
-//Obtiene la respuesta del formulario y comprueba si es correcto
-app.post('/login', function(req, res){
-    if (!req.body.username || !req.body.password) {
-      console.log('login failed');
-        res.render('formulario/noautentificado' );
-    } else if(req.body.username in users &&
-              bcrypt.compareSync(req.body.password, users[req.body.username])) {
-      req.session.user = req.body.username;
-      req.session.admin = true;
-      res.render('formulario/autentificado' );
-    } else {
-      console.log('login failed');
-      res.render('formulario/noautentificado');
-    }
-  });
   //Borra la sesion.
   app.get('/logout', function (req, res) {
     req.session.destroy();
